6c5bbf9ad03927be1d187824e35581bd38ce48ce
Mario Sepúlveda <sepulvedamario87@gmail.com>
2024-07-02 00:33:09 -0500

    active env

--- src/app/(dashboard)/properties/propertiesList/page.tsx
+++ src/app/(dashboard)/properties/propertiesList/page.tsx
@@ -1,25 +1,27 @@
 // Component Imports
 import PropertyList from '@views/property/list'
 
-// const getData = async () => {
-//   // Vars
-//   const res = await fetch(`${process.env.API_URL}/apps/property-list`)
+const getData = async () => {
 
-//   if (!res.ok) {
-//     throw new Error('Failed to fetch userData')
-//   }
+  // Vars
+  const res = await fetch(`${process.env.API_URL}/apps/property-list`)
 
-//   return res.json()
-// }
+  if (!res.ok) {
+    throw new Error('Failed to fetch userData')
+  }
+
+  return res.json()
+}
 
 const PropertyListApp = async () => {
+
   // Vars
-  //const data = await getData()
+  const data = await getData()
 
   // console.log(data);
 
 
-  return <PropertyList propertyData={[]} />
+  return <PropertyList propertyData={data} />
 }
 
 export default PropertyListApp
